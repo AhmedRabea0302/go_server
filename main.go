24c3b9860dd172e6c97862d2367b03481416a6e9
AhmedRabea0302 <arabea169@gmail.com>
2023-07-01 19:57:19 +0300

    Implemented Route '/' handler function

--- main.go
+++ main.go
@@ -7,7 +7,17 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
 
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Welcome to Go First Http Request\n")
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
